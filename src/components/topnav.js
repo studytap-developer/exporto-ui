df3fd903efd2d3fea5832b17bc54b8cf82d52ef2
Niha Shaik <shaikniha321@gmail.com>
2023-08-14 10:39:14 +0530

    added maze image

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -1,17 +1,35 @@
+
 import React from 'react';
 import { TfiLocationPin } from 'react-icons/tfi';
 import { LuPhoneCall } from 'react-icons/lu';
 
 const Topnav = () => {
+  const handlePhoneClick = (phoneNumber) => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
   return (
-    <div className="sticky top-0 bg-white h-6 flex justify-between px-[5%] py-2 z-50">
+    <div className="sticky top-0 bg-white h-6 flex justify-between px-1
+    ">
       <div className="flex items-center px-1">
-        <TfiLocationPin className="text-green-900 mt-1 mr-1" />
+        <TfiLocationPin className="text-green-900 mt-1 mr-1" />  
         Hyderabad
       </div>
       <div className="flex items-center px-1">
         <LuPhoneCall className="text-green-900 mt-1 mr-2" />
-        +91 6301169527 , +91 7674946379 
+        <span
+          onClick={() => handlePhoneClick('916301169527')}
+          style={{ cursor: 'pointer' }}
+        >
+          +91 6301169527
+        </span>
+        ,{' '}
+        <span
+          onClick={() => handlePhoneClick('917674946379')}
+          style={{ cursor: 'pointer' }}
+        >
+          +91 7674946379
+        </span>
       </div>
     </div>
   );
